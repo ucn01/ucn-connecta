227b0e2a0f9c8f67c4513382951245ebf881edbd
Community Manager <cmanager@ucn.local>
2026-03-26 12:25:10 -0600

    fix: create-post page client

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import { CreatePost } from '@/components/feed/CreatePost';
 import { useUser } from '@/firebase';
 
 export default function CreatePostPage() {
     const { user } = useUser();
-  return (
-    <div className="max-w-2xl mx-auto">
-        <h1 className="text-3xl font-bold font-headline mb-6">Crear una publicación</h1>
-        {user && <CreatePost user={user} />}
-    </div>
-  );
+
+    return (
+        <div className="max-w-2xl mx-auto">
+            <h1 className="text-3xl font-bold font-headline mb-6">
+                Crear una publicación
+            </h1>
+            {user && <CreatePost user={user} />}
+        </div>
+    );
 }
